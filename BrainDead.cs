3f2abd1037e3c89f7c1fadbdea8082f4750631d8
culliiing <christian@culling.se>
2023-11-29 16:50:38 +0100

    Behaviour system that moves agent randomly

---
+++ BrainDead.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI_for_digital_games
+{
+    internal class BrainDead : IBehaviourSystem
+    {
+        public void Update(Agent body)
+        {
+            body.Move(GetRandomDirection());
+        }
+
+        public void Decide(Agent body)
+        {
+            
+        }
+
+        Vector2 GetRandomDirection()
+        {
+            Random random = new Random();
+            int x = random.Next(-1, 2);
+            int y = random.Next(-1, 2);
+            return new Vector2(x, y);
+        }
+    }
+}
