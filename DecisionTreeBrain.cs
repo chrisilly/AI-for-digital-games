8d5969630a74959c4785f89fccd9e4d71e2bcd30
Chris <103900975+culliiing@users.noreply.github.com>
2023-12-06 22:51:19 +0100

    Decision Tree Brain

---
+++ DecisionTreeBrain.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI_for_digital_games
+{
+    internal class DecisionTreeBrain : IBehaviourSystem
+    {
+        DecisionTree decisionTree;
+
+        public DecisionTreeBrain()
+        {
+            decisionTree = new DecisionTree();
+        }
+
+        public void Update(AgentHandler handler, Agent body, GameTime gameTime)
+        {
+            body.Move(gameTime);
+
+            decisionTree.ParseTree(handler, body);
+        }
+    }
+}
