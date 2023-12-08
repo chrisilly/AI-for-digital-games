e2b8f9c3b3a3c50abed0950e0c8d587050dcf040
Chris <103900975+culliiing@users.noreply.github.com>
2023-12-08 19:51:54 +0100

    Finite State Machine Brain

---
+++ FiniteStateMachineBrain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace AI_for_digital_games
+{
+    internal class FiniteStateMachineBrain : IBehaviourSystem
+    {
+        public void Update(AgentHandler handler, Agent body, GameTime gameTime)
+        {
+            body.Move(gameTime);
+            body.State.Handle(handler, body);
+            body.State.Update(handler, body, gameTime);
+        }
+    }
+}
