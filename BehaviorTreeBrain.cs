118cb692b4961dbafa981ac3fb9f2436840fb215
Chris <103900975+culliiing@users.noreply.github.com>
2023-12-10 19:59:30 +0100

    Behavior Tree Brain

---
+++ BehaviorTreeBrain.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace AI_for_digital_games
+{
+    internal class BehaviorTreeBrain : IBehaviourSystem
+    {
+        BehaviorTree behaviorTree;
+
+        public BehaviorTreeBrain()
+        {
+            behaviorTree = new BehaviorTree();
+        }
+
+        public void Update(AgentHandler handler, Agent body, GameTime gameTime)
+        {
+            if(!behaviorTree.IsTreeGenerated())
+                behaviorTree.GenerateTree(handler, body);
+
+            body.Move(gameTime);
+
+            behaviorTree.Tick();
+        }
+    }
+}
